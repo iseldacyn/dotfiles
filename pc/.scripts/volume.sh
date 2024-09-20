cf4ec4360123c7862d0da031130b160ddca89dad
iseldacyn <aielloise@gmail.com>
2024-09-20 03:05:55 -0400

    update volume for pc to be continuous

--- pc/.scripts/volume.sh
+++ pc/.scripts/volume.sh
@@ -10,23 +10,43 @@ function is_muted {
 
 case $1 in
     "percent")
-        if [ $(is_muted) = "yes" ] ; then
-            echo ""
-        else
-            volume=$(volume)
-            if [ "$volume" -ge 30 ] ; then
-                echo " $volume%"
+        while :
+        do
+            if [ "$(is_muted)" = "yes" ] ; then
+                echo ""
             else
-                echo " $volume%"
+                volume=$(volume)
+                if [ "$volume" -ge 30 ] ; then
+                    echo " $volume%"
+                else
+                    echo " $volume%"
+                fi
             fi
-        fi
+        done
         ;;
-    "mute")
-        pactl set-sink-mute @DEFAULT_SINK@ toggle
-        ;;
+
     "speakers")
         pactl set-default-sink alsa_output.usb-Razer_Razer_Nommo_Chroma-02.analog-stereo
         ;;
     "headphones")
         pactl set-default-sink alsa_output.usb-Jieli_Technology_UACDemoV1.0_1120022606020701-01.analog-stereo
+        ;;
+
+    "mute")
+        pactl set-sink-mute @DEFAULT_SINK@ toggle
+        ;;
+	"up_1")
+		if [ $(volume) -ge 100 ] ; then
+			:
+        else
+            pactl set-sink-volume @DEFAULT_SINK@ +1%
+        fi
+		;;
+	"up_5")
+        if [ "$(volume)" -ge 96 ] ; then
+            pactl set-sink-volume @DEFAULT_SINK@ +"$((100-$(volume)))"%
+        else
+            pactl set-sink-volume @DEFAULT_SINK@ +5%
+        fi
+		;;
 esac
