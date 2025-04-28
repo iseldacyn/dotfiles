a1eac3abd5b0ce3bb611b4cef07cdac2ed799a24
iselda aiello <iselda@cyndquil.com>
2025-04-28 17:16:40 -0400

    update screenshot to be more intuitive and featureful

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -3,7 +3,6 @@
 export GRIM_DEFAULT_DIR=/home/iselda/pictures/screenshots
 
 case $1 in
-
     0) # perform whole screen screenshot
         grim - | wl-copy -t image/png
         ;;
@@ -11,11 +10,12 @@ case $1 in
     1) # perform selection screenshot
         grim -g "$(slurp)" - | wl-copy -t image/png
         ;;
-
 esac
+zenity --notification --text="Screenshot saved to clipboard!"
 
-zenity --notification --text='Screenshot saved to clipboard!'
 # prompt and write to screenshot dir
-if zenity --text="Would you like to save to $GRIM_DEFAULT_DIR?" --question; then
-    wl-paste >$GRIM_DEFAULT_DIR/$(date '+%Y-%m-%d_%Hh%Mm%Ss')_grim.png
+if zenity --text="Would you like to save to a file?" --question; then
+    default_name=$GRIM_DEFAULT_DIR/$(date '+%Y-%m-%d_%Hh%Mm%Ss')_grim.png
+    output_name=$(zenity --entry --text="Where would you like to save to?" --entry-text="$default_name")
+    wl-paste >$output_name
 fi
