350ca4bac79c4933305e6f2d1c512a9e0309414b
iselda aiello <iselda@cyndquil.com>
2025-04-29 09:12:42 -0400

    change initial screenshot name

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -15,7 +15,7 @@ zenity --notification --text="Screenshot saved to clipboard!"
 
 # prompt and write to screenshot dir
 if zenity --text="Would you like to save to a file?" --question; then
-    default_name=$GRIM_DEFAULT_DIR/$(date '+%Y-%m-%d_%Hh%Mm%Ss')_grim.png
+    default_name=$GRIM_DEFAULT_DIR/$(date '+%Y%m%d_%Hh%Mm%Ss')_grim.png
     output_name=$(zenity --entry --text="Where would you like to save to?" --entry-text="$default_name")
     wl-paste >$output_name
 fi
